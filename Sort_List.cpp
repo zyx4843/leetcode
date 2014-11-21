f78d6853fd4dac15cb19584d812883e0902cb21f
zhengyixiong <zyx4843@163.com>
2014-11-21 14:16:10 +0800

    Update Sort_List.cpp

--- Sort_List.cpp
+++ Sort_List.cpp
@@ -7,14 +7,13 @@
 * Sort a linked list in O(n log n) time using constant space complexity.
 *               
 **********************************************************************************/
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
   void quitSortList(ListNode *&begin, ListNode *&end)
